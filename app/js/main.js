d93e499dccb94cbfc3918e67f6e6c95f0b05a65d
TatyanaSimchenko <misstaty8181@gmail.com>
2020-07-31 01:25:04 +0300

    added weekly end newest sliders

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,25 @@
 $(function(){
 
+  $(".rate-star").rateYo({
+    rating: 4.6,
+    readOnly: true,
+    starWidth: "15px",
+    normalFill: "#fff",
+    ratedFill: "#ffc000"
+    
+  });
+
+  $('.weekly-slider__inner').slick({
+    dots:false,
+        arrows: true,
+       nextArrow: '<button class="slick-arrow slick-next"><img src="images/chevron-right.svg" alt="next arrow"></button>',
+      prevArrow: '<button class="slick-arrow slick-prev"><img src="images/chevron-left.svg" alt="prev arrow"></button>',
+  fade:true
+    
+});
+
+  
+var mixer = mixitup('.newest-slider__wrapper');
+
 
 });
